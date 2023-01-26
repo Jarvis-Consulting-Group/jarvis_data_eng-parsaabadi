78fe3983f1e2dabecaa35dba50a9b7551a51a1c3
Parsa Abadi <parsaabadi22@gmail.com>
2023-01-26 18:23:46 +0000

    fixed ddl.sql spelling error

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -22,10 +22,10 @@ CREATE TABLE IF NOT EXISTS PUBLIC.host_usage
      "timestamp"    TIMESTAMP NOT NULL,
      host_id        SERIAL NOT NULL,
      memory_free    INT4 NOT NULL,
-     cpu_idel       INT2 NOT NULL,
+     cpu_idle       INT2 NOT NULL,
      cpu_kernel     INT2 NOT NULL,
      disk_io        INT4 NOT NULL,
      disk_available INT4 NOT NULL,
      CONSTRAINT host_usage_host_info_fk FOREIGN KEY (host_id) REFERENCES
      host_info(id)
-  );
+ );
